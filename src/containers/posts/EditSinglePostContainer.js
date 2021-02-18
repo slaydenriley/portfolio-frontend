26955203a2ec0ae37d45d7a3b88e30f2b7db7074
Riley Slayden <slaydenriley@gmail.com>
2021-02-18 08:49:12 -0700

    Removed unused files and dependancies

--- src/containers/posts/EditSinglePostContainer.js
+++ src/containers/posts/EditSinglePostContainer.js
@@ -4,9 +4,7 @@ import fetchPosts from '../../actions/fetchPosts'
 import editPost from '../../actions/editPost'
 import fetchSinglePost from '../../actions/fetchSinglePost'
 import fetchTags from '../../actions/fetchTags'
-import {stateToHTML} from 'draft-js-export-html';
 import PostEditor from '../../components/posts/PostEditor'
-import { Editor } from "react-draft-wysiwyg";
 import { BlockReserveLoading } from 'react-loadingg';
 import { Redirect} from 'react-router-dom'
 import ReactQuill from 'react-quill';
